cecb985abae2460862a5ff4aa82dcdda708ef0ea
Harry <akhilesh@gmail.com>
2023-02-11 13:02:30 +0530

    corousel and product card

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../styles/App.css';
 import Header from './Header/Header';
+import Homescreen from './Homescreen/Homescreen';
 const App = () => {
 
   return (
     <div id="main">
       <Header />
+      <Homescreen />
     </div>
   )
 }
